6ead7a40be19ad8ea66998fb5c9faacb338f8f94
Your Name <you@example.com>
2020-02-17 10:57:54 +0500

    Written actual game. Remodeled everything.

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import pygame
 import os
 import random
+import sys
+
+
+AVAILABLE_SPAWNS = {1, 2, 3, 4}
 
 
 def load_image(name, colorkey=None):
@@ -11,35 +15,141 @@ def load_image(name, colorkey=None):
             colorkey = image.get_at((0, 0))
         image.set_colorkey(colorkey)
     else:
-        image = image.convert_alpha()
+        image = image.convert()
     return image
 
 
 class Car(pygame.sprite.Sprite):
     def __init__(self, group):
         super().__init__(group)
+        global AVAILABLE_SPAWNS
+        self.image = load_image('car1.png', -1)
         self.rect = self.image.get_rect()
-        self.rect.x = 0
-        self.rect.y = 0
-        self.image = load_image(random.choice(('car1.png', 'car2.png', 'car3.png')))
+        self.row = 0
+        self.row = random.choice(list(AVAILABLE_SPAWNS))
+        AVAILABLE_SPAWNS.remove(self.row)
+        self.rect.y = self.y = 0 - self.rect.height
+        self.on_cooldown = True
+        if self.row == 1:
+            self.x = self.rect.x = 95
+            self.speed = 50
+            self.spawn_cooldown = 2
+        elif self.row == 2:
+            self.x = self.rect.x = 185
+            self.speed = 50
+            self.spawn_cooldown = 2
+        elif self.row == 3:
+            self.x = self.rect.x = 290
+            self.speed = 100
+            self.image = pygame.transform.flip(self.image, 0, 1)
+            self.spawn_cooldown = 1
+        elif self.row == 4:
+            self.x = self.rect.x = 380
+            self.speed = 100
+            self.image = pygame.transform.flip(self.image, 0, 1)
+            self.spawn_cooldown = 1
 
     def update(self, dt):
-        self.y += 10 * dt / 1000
+        global AVAILABLE_SPAWNS
+        if self.spawn_cooldown >= 0:
+            if self.spawn_cooldown - dt <= 0 and self.on_cooldown:
+                AVAILABLE_SPAWNS.add(self.row)
+                self.on_cooldown = False
+            else:
+                self.spawn_cooldown -= dt
+        self.y += self.speed * dt
+        self.rect.y = int(self.y)
 
 
-class Player(Car):
+class Player(pygame.sprite.Sprite):
     def __init__(self, group):
         super().__init__(group)
-        self.image = load_image('player.png')
+        self.image = pygame.transform.scale(load_image('player.png', -1), (64, 96))
+        self.rect = self.image.get_rect()
+        self.rect.x = 90
+        self.rect.y = 380
+        self.x = 90
+        self.y = 380
+        self.pressed_keys = set()
+        self.outofroad_timeout = 5
+
+    def update(self, dt):
+        self.rect.y = int(self.y)
+        self.rect.x = int(self.x)
+        for key in self.pressed_keys:
+            if key == pygame.K_w and self.y > 10:
+                self.y -= 10
+            elif key == pygame.K_s and self.y + self.rect.height < 470:
+                self.y += 10
+            elif key == pygame.K_a and self.x > 40:
+                self.x -= 10
+            elif key == pygame.K_d and self.x + self.rect.width < 500:
+                self.x += 10
+        if self.x < 50 or self.x > 436:
+            self.outofroad_timeout -= dt
+        else:
+            self.outofroad_timeout = 5
 
 
-width, height = 480, 540
-pygame.init()
-screen = pygame.display.set_mode((width, height))
-clock = pygame.time.Clock()
+def main():
+    width, height = 540, 480
+    pygame.init()
+    pygame.font.init()
+    FONT = pygame.font.SysFont('Calibri MS', 30)
+    screen = pygame.display.set_mode((width, height))
+    clock = pygame.time.Clock()
+    FPS = 30
+    SCORE = 0
+    stopped = False
+    all_sprites = pygame.sprite.Group()
+    player = Player(all_sprites)
+    background_image = load_image('background.png')
 
-running = True
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key in (pygame.K_w, pygame.K_a, pygame.K_s, pygame.K_d) and not stopped:
+                    player.pressed_keys.add(event.key)
+                elif event.key == pygame.K_ESCAPE:
+                    running = False
+                    pygame.quit()
+                    return int(SCORE)
+            elif event.type == pygame.KEYUP:
+                if event.key in (pygame.K_w, pygame.K_a, pygame.K_s, pygame.K_d):
+                    if event.key in player.pressed_keys:
+                        player.pressed_keys.remove(event.key)
+        screen.blit(background_image, (0, 0))
+        if not stopped:
+            all_sprites.update(1 / FPS)
+            for sprite in all_sprites:
+                if 480 <= sprite.y or 0 >= sprite.y + sprite.rect.height:
+                    all_sprites.remove(sprite)
+            if random.randint(0, len(all_sprites) * 10) == 0 and AVAILABLE_SPAWNS:
+                Car(all_sprites)
+        all_sprites.draw(screen)
+        if player.outofroad_timeout == 5:
+            if not stopped:
+                SCORE += 10 / FPS
+            for sprite in all_sprites:
+                if sprite == player:
+                    continue
+                for x, y in ((player.x + 10, player.y + 10),
+                             (player.x + 10, player.y + player.rect.height - 10), (player.x + player.rect.width - 10, player.y + 10),
+                             (player.x + player.rect.width - 10, player.y + player.rect.height - 10)):
+                    if sprite.x < x < sprite.x + sprite.rect.width and \
+                       sprite.y < y < sprite.y + sprite.rect.height:
+                        stopped = True
+        else:
+            if player.outofroad_timeout <= 0:
+                stopped = True
+            elif not stopped:
+                screen.blit(FONT.render('RETURN ON ROAD IN ' + str(int(player.outofroad_timeout)), False, (255, 0, 0)), (160, 232))
+        if stopped:
+            screen.blit(FONT.render('YOU LOST, PRESS ESC', False, (255, 0, 0)), (160, 232))
+        screen.blit(FONT.render('SCORE: ' + str(int(SCORE)), False, (255, 255, 255)), (0, 0))
+        pygame.display.flip()
+        clock.tick(FPS)
